d2a2980d4e0d45737d2b71b34714e0e352824dea
Muna Adan <adan0027@algonquinlive.com>
2022-03-10 16:21:12 -0500

    new commit

--- Day4/challenge2.js
+++ Day4/challenge2.js
@@ -23,3 +23,29 @@ switch (true) {
     console.log('you failed');
 }   
    
+
+let dayUserInput = prompt('What day is today ?')
+let day = dayUserInput.toLowerCase()
+
+switch (day) {
+  case 'monday':
+    console.log('Monday is a working day')
+    break
+  case 'tuesday':
+      console.log('Tuesday is a working day')
+      break
+      case 'wednesday':
+          console.log('wednesday is a working day')
+          break
+          case 'thursday':
+              console.log('thursday is a working day')
+              break
+              case 'friday':
+              console.log('firday is a working day')
+              break 
+              case 'saturday':
+                  console.log('saturday is a weekend')
+                  break 
+                  case 'sunday':
+                      console.log('sunday is a weekend')
+}
---
+++ Day4/challenge3.js
@@ -0,0 +1,42 @@
+let monthOfYear = prompt(`What month is it?`)
+let month = monthOfYear.toLocaleLowerCase()
+
+switch (month) {
+    case 'January':
+        console.log(`January has 31 days`)
+        break
+        case 'February':
+            console.log(`February has 28 Days in a month`)
+            break
+            case 'March':
+                console.log(`March has 31 days in a month`)
+                break
+                case 'April':
+                console.log(`April has 30 days in a month`)
+                break 
+                case 'May':
+                    console.log(`May has 31 days in a month`)
+                    break
+                    case 'June':
+                        console.log('June has 30 days in a month')
+                        break
+                        case 'July':
+                            console.log(`July has 31 days in a month`)
+                            break
+                            case 'August':
+                                console.log(`August has 31 days in a month`)
+                                break
+                                case 'September':
+                                console.log(`September has 30 days in a month`)
+                                break
+                                case 'October':
+                                    console.log(`October has 31 days in a month`)
+                                    break
+                                    case 'November':
+                                        console.log(`Novemeber has 30 days in a month`)
+                                        break
+                                        case 'December':
+                                            console.log(`December has 31 days in a month`)
+
+}
+
